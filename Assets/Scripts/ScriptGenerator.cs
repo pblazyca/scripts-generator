4bed40269725a06cf286d480efd0a3b91a537c9e
pblazyca <pblazyca@outlook.com>
2021-01-25 23:29:05 +0100

    Test new class BasicGenerator.

--- Assets/Scripts/ScriptGenerator.cs
+++ Assets/Scripts/ScriptGenerator.cs
@@ -9,20 +9,27 @@ public class ScriptGenerator : MonoBehaviour
 
     protected virtual void Start()
     {
-        Generator generator = new Generator(Settings);
+        BasicGenerator generator = new BasicGenerator(Settings);
 
-        generator.WriteTextLine("nowy kawałek tekstu");
-        generator.BeginBlock();
-        generator.WriteTextLine("kawałek tekstu w bloku");
-        generator.WriteEmptyLine();
-        generator.EndBlock();
-        generator.WriteEmptyLine();
-        generator.WriteTextLine("tekst");
-        generator.WrtieText("pierwszy tekst");
-        generator.WrtieText(" drugi doklejony tekst");
-        generator.WriteEmptyLine();
-        generator.BeginBlock();
-        generator.EndBlock();
+        //generator.WriteTextLine("nowy kawałek tekstu");
+        //generator.BeginBlock();
+        //generator.WriteTextLine("kawałek tekstu w bloku");
+        //generator.WriteEmptyLine();
+        //generator.EndBlock();
+        //generator.WriteEmptyLine();
+        //generator.WriteTextLine("tekst");
+        //generator.WrtieText("pierwszy tekst");
+        //generator.WrtieText(" drugi doklejony tekst");
+        //generator.WriteEmptyLine();
+        //generator.BeginBlock();
+        //generator.EndBlock();
+
+        List<string> namespaces = new List<string>() { "System", "System.Collections", "UnityEngine.UI" };
+        generator.WriteNamespaceBlock(namespaces);
+
+        List<string> interfaces = new List<string>() { "IInterfaces", "IAwesomable" };
+        generator.BeginClass(AccessModifiers.PROTECTED_INTERNAL, "NewClass", "BaseGen", interfaces);
+        generator.EndClass();
 
         Debug.Log(generator.CodeBuilder.ToString());
     }

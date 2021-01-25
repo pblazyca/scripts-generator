a2ec48399320078c5bcc774d9dc3cf02e427e70c
pblazyca <pblazyca@outlook.com>
2021-01-25 23:28:51 +0100

    Added BasicGenerator implementation of Generator class. Added WriteNamespaceBlock, BeginClass, EndClass method.

---
+++ Assets/Scripts/Core/Generator/BasicGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class BasicGenerator : Generator
+{
+    public BasicGenerator(GeneratorSettings settings) : base(settings) { }
+
+    public void WriteNamespaceBlock(List<string> namespaceCollection)
+    {
+        string namespaceLine = string.Empty;
+
+        for (int i = 0; i < namespaceCollection.Count; i++)
+        {
+            namespaceLine = $"using {namespaceCollection[i]};";
+            WriteTextLine(namespaceLine);
+        }
+    }
+
+    public void BeginClass(AccessModifiers accessModifier, string className, string baseClassName = null, List<string> implementedInterfaceNameCollection = null)
+    {
+        string accessModifierLabel = GetAccessModifiersLabel(accessModifier);
+        string beginClassLine = $"{accessModifierLabel} class {className}";
+
+        if (baseClassName != null)
+        {
+            beginClassLine += $" : {baseClassName}";
+        }
+
+        if (implementedInterfaceNameCollection != null)
+        {
+            for (int i = 0; i < implementedInterfaceNameCollection.Count; i++)
+            {
+                beginClassLine += $", {implementedInterfaceNameCollection[i]}";
+            }
+        }
+
+        WriteEmptyLine();
+        WriteTextLine(beginClassLine);
+        BeginBlock();
+    }
+
+    public void EndClass()
+    {
+        WriteEmptyLine();
+        EndBlock();
+    }
+
+
+    private string GetAccessModifiersLabel(AccessModifiers accessModifier)
+    {
+        string label = accessModifier.ToString();
+        label = label.ToLower();
+        label = label.Replace(UNDERLINE, SPACE);
+
+        return label;
+    }
+}

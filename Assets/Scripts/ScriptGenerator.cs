174eff94a09ddd0b7084ecfab62e8c8f79bca254
pblazyca <pblazyca@outlook.com>
2021-07-01 00:41:14 +0200

    Updated demo class with new structures.

--- Assets/Scripts/ScriptGenerator.cs
+++ Assets/Scripts/ScriptGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using ScriptsGenerator.Core;
 using ScriptsGenerator.Structures;
@@ -27,11 +27,17 @@ namespace ScriptsGenerator.Demo
             //generator.BeginBlock();
             //generator.EndBlock();
 
-            List<string> namespaces = new List<string>() { "System", "System.Collections", "UnityEngine.UI" };
+            List<NamespaceInfo> namespaces = new List<NamespaceInfo>() { new NamespaceInfo("System"), new NamespaceInfo("System.Collections"), new NamespaceInfo("UnityEngine.UI") };
             generator.WriteNamespaceBlock(namespaces);
 
-            List<string> interfaces = new List<string>() { "IInterfaces", "IAwesomable" };
+            List<InterfaceInfo> interfaces = new List<InterfaceInfo>() { new InterfaceInfo("IInterfaces"), new InterfaceInfo("IAwesomable") };
             generator.BeginClass(AccessModifiers.PROTECTED_INTERNAL, "NewClass", "BaseGen", interfaces);
+
+            List<PropertyInfo> properties = new List<PropertyInfo>() { new PropertyInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(int), "a", "20")), new PropertyInfo(AccessModifiers.PROTECTED, new VariableInfo(typeof(int), "b", "52")), new PropertyInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(MethodInfo), "method")) };
+            generator.WriteProperty(properties);
+
+            List<FieldInfo> fields = new List<FieldInfo>() { new FieldInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(int), "a")), new FieldInfo(AccessModifiers.PROTECTED, new VariableInfo(typeof(int), "b", "0")), new FieldInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(MethodInfo), "method")) };
+            generator.WriteField(fields);
 
             List<VariableInfo> parameters = new List<VariableInfo>() { new VariableInfo(typeof(int), "number"), new VariableInfo(typeof(string), "label") };
             MethodInfo methodInfo = new MethodInfo(AccessModifiers.PRIVATE, typeof(void), "NewMethod", parameters);

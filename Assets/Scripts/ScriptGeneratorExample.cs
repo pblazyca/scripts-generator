1cf9f5d48dfe1a2ea46dd8a4cde2189859b0a11f
pblazyca <pblazyca@outlook.com>
2022-01-30 00:10:09 +0100

    Beautify example by empty lines added. Removed unused commented lines of code.

--- Assets/Scripts/ScriptGeneratorExample.cs
+++ Assets/Scripts/ScriptGeneratorExample.cs
@@ -14,30 +14,20 @@ namespace ScriptsGenerator.Demo
         {
             Core.ScriptGenerator generator = new Core.ScriptGenerator(Settings);
 
-            //generator.WriteTextLine("nowy kawałek tekstu");
-            //generator.BeginBlock();
-            //generator.WriteTextLine("kawałek tekstu w bloku");
-            //generator.WriteEmptyLine();
-            //generator.EndBlock();
-            //generator.WriteEmptyLine();
-            //generator.WriteTextLine("tekst");
-            //generator.WrtieText("pierwszy tekst");
-            //generator.WrtieText(" drugi doklejony tekst");
-            //generator.WriteEmptyLine();
-            //generator.BeginBlock();
-            //generator.EndBlock();
-
             List<UsingInfo> usings = new List<UsingInfo>() { new UsingInfo("System"), new UsingInfo("System.Collections"), new UsingInfo("UnityEngine.UI") };
             generator.WriteUsing(usings);
+            generator.WriteEmptyLine();
 
             NamespaceInfo namespaceInfo = new("Awesome.Inc");
             generator.BeginNamespace(namespaceInfo);
 
             List<InterfaceInfo> interfaces = new List<InterfaceInfo>() { new InterfaceInfo("IInterfaces"), new InterfaceInfo("IAwesomable") };
             generator.BeginClass(AccessModifiers.PROTECTED_INTERNAL, "NewClass", "BaseGen", interfaces);
+            generator.WriteEmptyLine();
 
             List<PropertyInfo> properties = new List<PropertyInfo>() { new PropertyInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(int), "a", "20")), new PropertyInfo(AccessModifiers.PROTECTED, new VariableInfo(typeof(int), "b", "52")), new PropertyInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(MethodInfo), "method")) };
             generator.WriteProperty(properties);
+            generator.WriteEmptyLine();
 
             List<FieldInfo> fields = new List<FieldInfo>() { new FieldInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(int), "a")), new FieldInfo(AccessModifiers.PROTECTED, new VariableInfo(typeof(int), "b", "0")), new FieldInfo(AccessModifiers.PRIVATE, new VariableInfo(typeof(MethodInfo), "method")) };
             generator.WriteField(fields);
@@ -46,11 +36,13 @@ namespace ScriptsGenerator.Demo
             MethodInfo methodInfo = new MethodInfo(AccessModifiers.PRIVATE, typeof(void), "NewMethod", parameters);
 
             generator.BeginMethod(methodInfo);
+            generator.WriteEmptyLine();
             generator.EndMethod();
 
             parameters = new List<VariableInfo>() { new VariableInfo(typeof(int), "number") };
             methodInfo = new MethodInfo(AccessModifiers.PRIVATE, typeof(void), "NewVirtualMethod", PolymorphismKeyword.ABSTRACT, parameters);
 
+            generator.WriteEmptyLine();
             generator.BeginMethod(methodInfo);
 
             generator.EndClass();

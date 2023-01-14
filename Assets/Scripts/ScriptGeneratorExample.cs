33cf7a3896fd8983b136f155b29222c720ee3bc7
pblazyca <pblazyca@outlook.com>
2023-01-14 21:44:34 +0100

    A demo class was cleaned up.

--- Assets/Scripts/ScriptGeneratorExample.cs
+++ Assets/Scripts/ScriptGeneratorExample.cs
@@ -30,45 +30,77 @@ namespace ScriptsGenerator.Demo
 
         private void GenerateSampleClass()
         {
-            List<UsingInfo> usings = new() { new("System"), new("System.Collections"), new("UnityEngine.UI") };
-            Generator.WriteUsing(usings);
-            Generator.WriteEmptyLine();
+            GenerateUsings();
+            GenerateBeginClass();
 
+            GenerateClassProperties();
+            GenerateClassFields();
+
+            GenerateMethod();
+            GenerateAbstractMethod();
+
+            GenerateNestedClass();
+
+            Generator.WriteEmptyLine();
+            Generator.EndClass();
+            Generator.EndNamespace();
+        }
+
+        private void GenerateBeginClass()
+        {
             NamespaceInfo namespaceInfo = new("Awesome.Inc");
             Generator.BeginNamespace(namespaceInfo);
 
             List<InterfaceInfo> interfaces = new() { new("IInterfaces"), new("IAwesomable") };
             Generator.BeginClass(AccessModifiers.PROTECTED_INTERNAL, "NewClass", "BaseGen", interfaces);
             Generator.WriteEmptyLine();
+        }
 
+        private void GenerateUsings()
+        {
+            List<UsingInfo> usings = new() { new("System"), new("System.Collections"), new("UnityEngine.UI") };
+            Generator.WriteUsing(usings);
+            Generator.WriteEmptyLine();
+        }
+
+        private void GenerateClassProperties()
+        {
             List<PropertyInfo> properties = new() { new(AccessModifiers.PRIVATE, new(typeof(int), "a", "20")), new(AccessModifiers.PROTECTED, new(typeof(int), "b", "52")), new(AccessModifiers.PRIVATE, new(typeof(MethodInfo), "method")) };
             Generator.WriteProperty(properties);
             Generator.WriteEmptyLine();
+        }
 
+        private void GenerateClassFields()
+        {
             List<FieldInfo> fields = new() { new(AccessModifiers.PRIVATE, new(typeof(int), "a")), new(AccessModifiers.PROTECTED, new(typeof(int), "b", "0")), new(AccessModifiers.PRIVATE, new(typeof(MethodInfo), "method")) };
             Generator.WriteField(fields);
+        }
 
+        private void GenerateMethod()
+        {
             List<VariableInfo> parameters = new() { new(typeof(int), "number"), new(typeof(string), "label") };
             MethodInfo methodInfo = new(AccessModifiers.PRIVATE, typeof(void), "NewMethod", parameters);
 
             Generator.BeginMethod(methodInfo);
             Generator.WriteEmptyLine();
             Generator.EndMethod();
+        }
 
-            parameters = new() { new(typeof(int), "number") };
-            methodInfo = new(AccessModifiers.PRIVATE, typeof(void), "NewVirtualMethod", PolymorphismKeyword.ABSTRACT, parameters);
+        private void GenerateAbstractMethod()
+        {
+            List<VariableInfo> parameters = new() { new(typeof(int), "number") };
+            MethodInfo methodInfo = new(AccessModifiers.PRIVATE, typeof(void), "NewVirtualMethod", PolymorphismKeyword.ABSTRACT, parameters);
 
             Generator.WriteEmptyLine();
             Generator.WriteAbstractMethod(methodInfo);
+        }
 
-            Generator.WriteEmptyLine();
+        private void GenerateNestedClass()
+        {
             Generator.BeginClass(AccessModifiers.PRIVATE, "NestedClass");
             Generator.WriteEmptyLine();
-            Generator.EndClass();
-
             Generator.WriteEmptyLine();
             Generator.EndClass();
-            Generator.EndNamespace();
         }
 
         private void ShowGenerationResult()
